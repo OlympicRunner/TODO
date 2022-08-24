28d91d28151ba0f76db5f42c7ce1be39407ee8ed
Rostyslav <novijrost@gmail.com>
2022-08-24 17:03:53 +0300

    add disabled for button

--- todo-app.js
+++ todo-app.js
@@ -11,12 +11,12 @@
         let input = document.createElement('input')
         let buttonWrapper = document.createElement('div')
         let button = document.createElement('button')
-    
         form.classList.add('input-group', 'mb-3')
         input.classList.add('form-control')
         input.placeholder = 'Введите название нового дела'
         buttonWrapper.classList.add('input-group-append')
         button.classList.add('btn', 'btn-primary')
+        button.disabled = true
         button.textContent = 'Добавить дело'
     
         buttonWrapper.append(button)
@@ -64,24 +64,70 @@
         }
     }
     
+    
+    function createTodoApp(container, title = 'Список дел') {
 
 
 
-
-
-    document.addEventListener('DOMContentLoaded', function() {
-        let container = document.getElementById('todo-app')
-
-        let todoAppTitle = createAppTitle('Список дел')
+        let todoAppTitle = createAppTitle(title)
         let todoItemForm = createTodoItemForm();
         let todoList = createTodoList()
 
         container.append(todoAppTitle)
         container.append(todoItemForm.form)
         container.append(todoList)
+        
+        
 
+        todoItemForm.form.addEventListener('submit', function(e) {
+
+            // button.disabled = false
+
+            e.preventDefault()
+
+
+            if(!todoItemForm.input.value) {
+                return;  
+            } 
+
+
+            let todoItem = createTodoItem(todoItemForm.input.value) 
+
+            todoItem.doneButton.addEventListener('click', function() {
+                todoItem.item.classList.toggle('list-group-item-success')
+            })
+
+            todoItem.deleteButton.addEventListener('click', function() {
+                if (confirm('Вы уверены?')) {
+                    todoItem.item.remove()
+                }
+            })
+
+            todoList.append(todoItem.item)
+
+
+            todoItemForm.input.value = ''
+
+            button.textContent = 'ads'
+        })
+        
+    }
+
+    window.createTodoApp = createTodoApp
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        let input = document.querySelector('input')
+        let button = document.querySelector('.btn')
+
+        input.addEventListener('input', function () {
+            if (input.value === '') {
+                button.disabled = true
+            } else {
+                button.disabled = false
+            }
+        })
     })
-
 })();
 
 
